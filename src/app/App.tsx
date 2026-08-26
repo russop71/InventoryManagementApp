af797e397872d630b7bbb2a30bb48403ad22205e
russop71 <russop71@gmail.com>
2026-08-26 10:35:02 -0400

    Add waste tracking: update src/app/App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,18 +4,21 @@ import { InventoryProvider } from './contexts/InventoryContext';
 import { ToastProvider } from './contexts/ToastContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { LaborProvider } from './contexts/LaborContext';
+import { WasteProvider } from './contexts/WasteContext';
 import { Toaster } from './components/ui/sonner';
 
 function App() {
   return (
     <AuthProvider>
       <InventoryProvider>
-        <LaborProvider>
-          <ToastProvider>
-            <RouterProvider router={router} />
-            <Toaster />
-          </ToastProvider>
-        </LaborProvider>
+        <WasteProvider>
+          <LaborProvider>
+            <ToastProvider>
+              <RouterProvider router={router} />
+              <Toaster />
+            </ToastProvider>
+          </LaborProvider>
+        </WasteProvider>
       </InventoryProvider>
     </AuthProvider>
   );
